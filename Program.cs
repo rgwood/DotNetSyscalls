8fbf45f29fe33bba989d7b2b01ab192c9bd16ddf
Reilly Wood <reilly.wood@icloud.com>
2020-02-11 03:26:59 +0000

    Remove unnecessary namespace, refactor Mono call

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace syscalls
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
-        {
-            var pid = GetPidUsingPInvoke();
-            Console.WriteLine($"My PID from P/Invoke: {pid}");
-
-            pid = GetPidUsingNativeLibrary();
-            Console.WriteLine($"My PID from NativeLibrary: {pid}");
-
-            pid = Mono.Unix.Native.Syscall.getpid();
-            Console.WriteLine($"My PID from Mono.Posix.NETStandard: {pid}");
-        }
-
-        [DllImport("libc", EntryPoint = "getpid")]
-        private static extern int GetPidUsingPInvoke();
-
-        private delegate int GetPidDelegate();
-        private static int GetPidUsingNativeLibrary()
-        {
-            IntPtr libraryPtr = NativeLibrary.Load("libc");
-            IntPtr symbolPtr = NativeLibrary.GetExport(libraryPtr, "getpid");
-            var getPidDelegate = Marshal.GetDelegateForFunctionPointer<GetPidDelegate>(symbolPtr);
-            return getPidDelegate();
-        }
+        var pid = GetPidUsingPInvoke();
+        Console.WriteLine($"My PID from P/Invoke: {pid}");
+        pid = GetPidUsingNativeLibrary();
+        Console.WriteLine($"My PID from NativeLibrary: {pid}");
+        pid = GetPidUsingMonoLibrary();
+        Console.WriteLine($"My PID from Mono.Posix.NETStandard: {pid}");
     }
+    [DllImport("libc", EntryPoint = "getpid")]
+    private static extern int GetPidUsingPInvoke();
+    private delegate int GetPidDelegate();
+    private static int GetPidUsingNativeLibrary()
+    {
+        IntPtr libraryPtr = NativeLibrary.Load("libc");
+        IntPtr symbolPtr = NativeLibrary.GetExport(libraryPtr, "getpid");
+        var getPidDelegate = Marshal.GetDelegateForFunctionPointer<GetPidDelegate>(symbolPtr);
+        return getPidDelegate();
+    }
+    private static int GetPidUsingMonoLibrary() => Mono.Unix.Native.Syscall.getpid();
 }
